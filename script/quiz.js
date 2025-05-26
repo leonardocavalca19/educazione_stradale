0494eb637fd5e5270548266afc005a476e787868
salvatore frattaruolo <ssalvatore.frattaruolo@itis.pr.it>
2025-05-26 17:08:52 +0200

    fix creazione oggetto

--- script/quiz.js
+++ script/quiz.js
@@ -9,6 +9,16 @@ async function getutenti() {
     for (let i = 0; i < datiJSON.length; i++) {
       utenti.push(new Utente(datiJSON[i].nome, datiJSON[i].cognome, datiJSON[i].email, datiJSON[i].passwordHash, datiJSON[i].dataNascita, datiJSON[i].test))
     }
+    for (let i = 0; i < utenti.length; i++) {
+            if (utenti[i].test != null){
+                let tests=[]
+                for (let j = 0; j < utenti[i].test.length; j++) {
+                    tests.push(new Test(utenti[i].test[j].domande));
+                    tests[j].realizazzione = utenti[i].test[j].realizazzione;
+                }
+                utenti[i].test = tests;
+            }
+        }
 
   } catch (error) {
     console.error("Impossibile caricare il file utenti.json:", error);
