c799186ce4ba15cfaa3be6ad182ae9710075b259
salvatore frattaruolo <ssalvatore.frattaruolo@itis.pr.it>
2025-06-01 15:09:54 +0200

    finale

--- script/risultati.js
+++ script/risultati.js
@@ -195,10 +195,14 @@ async function crea() {
         let errate = []
         let vero = 0
         let falso = 0
-        for (let i = 0; i < quiz.domande.length; i++) {
+        try {
+            for (let i = 0; i < quiz.domande.length; i++) {
             if (!quiz.domande[i].controllagiusta()) {
                 errate.push(quiz.domande[i])
             }
+        }
+        } catch (error) {
+            
         }
         let n = 0
         // Determina se l'utente è promosso o bocciato in base al numero di errori.
